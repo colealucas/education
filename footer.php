95c5530ec298663a39d240638da08474f978205b
colealucas <colealucas@gmail.com>
2024-07-28 15:27:35 +0300

    updated footer

--- footer.php
+++ footer.php
@@ -11,9 +11,9 @@
 
 ?>
 
-<footer id="site-footer" class="site-footer">
+<footer id="site-footer" class="site-footer bg-green rounded-12px m-8px py-26px px-50px text-white">
 	<div class="container">
-		<div class="text-center">
+		<div class="text-center copyright text-14px">
 			© <?php echo date('Y'); ?>, CJI. <?php _e('All Rights Reserved', 'education'); ?>.
 		</div>
 	</div>
