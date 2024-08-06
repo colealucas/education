4cfdc739172995c7e9174a6ce21a7bb8a82ec95c
colealucas <colealucas@gmail.com>
2024-08-06 16:33:00 +0300

    updated single book template

--- single-book.php
+++ single-book.php
@@ -1,4 +1,34 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * Single book template
+ */
+
+get_header(); 
+?>
+
+<div class="section py-48px bg-faded-gray rounded-48px mx-8px mt-16px manual-intro-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="max-w-[590px]">
+                    <h1 class="text-87px font-700 leading-110"><?php echo get_field('book_title'); ?></h1>
+
+                    <div class="my-16px text-16px">
+                        <?php echo get_field('book_short_description'); ?>
+                    </div>
+                </div> 
+            </div>
+            <div class="col-md-6">
+                <div class="manual-cover md:text-right">
+                    <?php if ( get_field('book_cover_image') ) : ?>
+                        <img src="<?php echo get_field('book_cover_image'); ?>" alt="">
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="book-content">
     <?php while (have_posts()) : the_post(); ?>
         <h1><?php the_title(); ?></h1>
@@ -26,4 +56,5 @@
         </ul>
     <?php endwhile; ?>
 </div>
+
 <?php get_footer(); ?>
