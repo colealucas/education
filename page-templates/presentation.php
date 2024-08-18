c3c760050048aca8b75337f6feb1733df9e7558a
colealucas <colealucas@gmail.com>
2024-08-18 23:04:16 +0300

    save reponsive

--- page-templates/presentation.php
+++ page-templates/presentation.php
@@ -2,6 +2,7 @@
 /**
  * Template name: Prezentare
  * 
+ * Placeholder image api url https://via.placeholder.com/685x503
  */
 
  get_header(); 
@@ -11,122 +12,51 @@
     <div class="container">        
         <div class="row items-center">
             <div class="col-md-6">
-                <h1 class=" text-60px font-700 leading-120 text-green">Centrul pentru Jurnalism Independent</h1>
+                <h1 class="text-36px md:text-50px lg:text-64px xl:text-[80px] font-700 leading-110 text-green"><?php echo get_field('page_title'); ?></h1>
 
-                <div class="text-16px mt-16px">
-                    Centrul pentru Jurnalism Independent (CJI) este prima organizație de media din Republica Moldova, fondată în 1994, ca un proiect al Casei „Lumea Deschisă”. În 1998 devine organizație independentă. Sub aspect juridic, CJI este o organizație nonguvernamentală (ONG), necomercială și apolitică.
+                <div class="text-16px mt-16px mb-30px">
+                    <?php echo get_field('page_description'); ?>
                 </div>
             </div>
             <div class="col-md-6">
-                <img class="rounded-32px" src="https://via.placeholder.com/685x503" alt="Imagine Placeholder">
+                <?php if ( get_field('featured_image') ) : ?>
+                    <div>
+                        <img class="rounded-32px" src="<?php echo get_field('featured_image'); ?>" alt="">
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
 </section>
 
 <section class="team-section py-32px">
-    <div class="container">        
-        <div class=" bg-faded-gray rounded-24px p-24px">
-            <h3 class="flex items-center text-18px mb-8px">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <g clip-path="url(#clip0_92_1751)">
-                        <path d="M17.625 7.87499C17.2359 7.87556 16.8518 7.96199 16.5 8.12812V4.87499C16.4975 4.17956 16.2202 3.51332 15.7284 3.02157C15.2367 2.52982 14.5704 2.25246 13.875 2.24999C13.4361 2.24877 13.004 2.35841 12.6188 2.56874C12.4242 1.97677 12.0254 1.47331 11.4937 1.1484C10.962 0.823486 10.3321 0.698313 9.71654 0.795265C9.10101 0.892216 8.54004 1.20497 8.13396 1.67759C7.72787 2.15021 7.50316 2.75188 7.5 3.37499V4.00312C7.09988 3.81332 6.65824 3.72784 6.2162 3.75463C5.77416 3.78142 5.34608 3.91961 4.97181 4.15634C4.59754 4.39307 4.28924 4.72064 4.07561 5.10856C3.86198 5.49648 3.74997 5.93214 3.75 6.37499V14.25C3.75 16.438 4.61919 18.5364 6.16637 20.0836C7.71354 21.6308 9.81196 22.5 12 22.5C14.188 22.5 16.2865 21.6308 17.8336 20.0836C19.3808 18.5364 20.25 16.438 20.25 14.25V10.5C20.2475 9.80456 19.9702 9.13832 19.4784 8.64657C18.9867 8.15482 18.3204 7.87746 17.625 7.87499ZM18.75 14.25C18.75 16.0402 18.0388 17.7571 16.773 19.023C15.5071 20.2888 13.7902 21 12 21C10.2098 21 8.4929 20.2888 7.22703 19.023C5.96116 17.7571 5.25 16.0402 5.25 14.25V6.37499C5.25 6.07662 5.36853 5.79048 5.5795 5.5795C5.79048 5.36852 6.07663 5.24999 6.375 5.24999C6.67337 5.24999 6.95952 5.36852 7.1705 5.5795C7.38147 5.79048 7.5 6.07662 7.5 6.37499V10.5C7.5 10.6989 7.57902 10.8897 7.71967 11.0303C7.86032 11.171 8.05109 11.25 8.25 11.25C8.44891 11.25 8.63968 11.171 8.78033 11.0303C8.92098 10.8897 9 10.6989 9 10.5V3.37499C9 3.07662 9.11853 2.79048 9.3295 2.5795C9.54048 2.36852 9.82663 2.24999 10.125 2.24999C10.4234 2.24999 10.7095 2.36852 10.9205 2.5795C11.1315 2.79048 11.25 3.07662 11.25 3.37499V9.74999C11.25 9.9489 11.329 10.1397 11.4697 10.2803C11.6103 10.421 11.8011 10.5 12 10.5C12.1989 10.5 12.3897 10.421 12.5303 10.2803C12.671 10.1397 12.75 9.9489 12.75 9.74999V4.87499C12.75 4.57662 12.8685 4.29048 13.0795 4.0795C13.2905 3.86852 13.5766 3.74999 13.875 3.74999C14.1734 3.74999 14.4595 3.86852 14.6705 4.0795C14.8815 4.29048 15 4.57662 15 4.87499V10.875C15 11.0739 15.079 11.2647 15.2197 11.4053C15.3603 11.546 15.5511 11.625 15.75 11.625C15.9489 11.625 16.1397 11.546 16.2803 11.4053C16.421 11.2647 16.5 11.0739 16.5 10.875V10.5C16.5 10.2016 16.6185 9.91548 16.8295 9.7045C17.0405 9.49352 17.3266 9.37499 17.625 9.37499C17.9234 9.37499 18.2095 9.49352 18.4205 9.7045C18.6315 9.91548 18.75 10.2016 18.75 10.5V14.25ZM12 12.75C9.49687 12.75 7.96875 14.6812 7.425 15.5062C7.30518 15.6904 7.24139 15.9053 7.24139 16.125C7.24139 16.3447 7.30518 16.5596 7.425 16.7437C7.96875 17.5687 9.49687 19.5 12 19.5C14.5031 19.5 16.0312 17.5687 16.575 16.7437C16.6948 16.5596 16.7586 16.3447 16.7586 16.125C16.7586 15.9053 16.6948 15.6904 16.575 15.5062C16.0312 14.6812 14.5031 12.75 12 12.75ZM12 17.625C11.7033 17.625 11.4133 17.537 11.1666 17.3722C10.92 17.2074 10.7277 16.9731 10.6142 16.699C10.5006 16.4249 10.4709 16.1233 10.5288 15.8324C10.5867 15.5414 10.7296 15.2741 10.9393 15.0643C11.1491 14.8546 11.4164 14.7117 11.7074 14.6538C11.9983 14.5959 12.2999 14.6256 12.574 14.7392C12.8481 14.8527 13.0824 15.045 13.2472 15.2916C13.412 15.5383 13.5 15.8283 13.5 16.125C13.5 16.5228 13.342 16.9043 13.0607 17.1857C12.7794 17.467 12.3978 17.625 12 17.625Z" fill="#00665E"/>
-                    </g>
-                    <defs>
-                        <clipPath id="clip0_92_1751">
-                        <rect width="24" height="24" fill="white"/>
-                        </clipPath>
-                    </defs>
-                </svg>
-                <span class="ml-4px inline-block text-18px font-600">
-                    Viziune
-                </span>
-            </h3>
+    <div class="container">
+        <?php if( have_rows('info_boxes') ): ?>
+            <?php while( have_rows('info_boxes') ): the_row(); 
+                $icon = get_sub_field('icon');
+                $title = get_sub_field('title');
+                $content = get_sub_field('content');
+            ?>
+                
+                <div class="bg-faded-gray rounded-24px p-24px mb-16px">
+                    <h3 class="flex items-center text-18px mb-8px">
+                        <?php if ($icon) : ?>
+                            <img src="<?php echo $icon; ?>" alt="">
+                        <?php endif; ?>
+                        
+                        <span class="ml-4px inline-block text-18px font-600">
+                            <?php echo $title; ?>
+                        </span>
+                    </h3>
 
-            <div class=" text-16px font-400">
-                Cetățeni bine informați de către o presă liberă și profesionistă.
-            </div>
-        </div>
+                    <div class="text-16px text-dark text-content">
+                        <?php echo $content; ?>
+                    </div>
+                </div>
+                
+            <?php endwhile; ?>
+        <?php endif; ?>
 
-        <div class=" bg-faded-gray rounded-24px p-24px mt-16px">
-            <h3 class="flex items-center text-18px mb-8px">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <g clip-path="url(#clip0_92_1759)">
-                    <path opacity="0.2" d="M12 16.5C14.4853 16.5 16.5 14.4853 16.5 12C16.5 9.51472 14.4853 7.5 12 7.5C9.51472 7.5 7.5 9.51472 7.5 12C7.5 14.4853 9.51472 16.5 12 16.5Z" fill="#00665E"/>
-                    <path d="M12 12L21 3" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M18.3656 5.63436C16.7701 4.03401 14.6318 3.09098 12.3741 2.99192C10.1165 2.89287 7.90386 3.64501 6.17427 5.09947C4.44468 6.55394 3.32416 8.60473 3.03448 10.8459C2.7448 13.0872 3.30708 15.3554 4.61003 17.2019C5.91298 19.0483 7.86163 20.3383 10.0703 20.8165C12.279 21.2947 14.5867 20.9263 16.5367 19.7842C18.4868 18.6422 19.937 16.8096 20.6003 14.6493C21.2636 12.489 21.0918 10.1584 20.1187 8.11874" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M15.1781 8.82187C14.4349 8.08073 13.4555 7.62335 12.4102 7.52917C11.3648 7.43499 10.3195 7.70996 9.45574 8.30633C8.59201 8.9027 7.96448 9.78277 7.68213 10.7937C7.39978 11.8046 7.48047 12.8825 7.91017 13.8401C8.33987 14.7977 9.09143 15.5746 10.0343 16.0357C10.9772 16.4968 12.0518 16.6131 13.0715 16.3644C14.0912 16.1156 14.9916 15.5175 15.6162 14.674C16.2408 13.8305 16.5502 12.7948 16.4906 11.7469" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                    </g>
-                    <defs>
-                    <clipPath id="clip0_92_1759">
-                    <rect width="24" height="24" fill="white"/>
-                    </clipPath>
-                    </defs>
-                </svg>
-                <span class="ml-4px inline-block text-18px font-600">
-                    Misiune
-                </span>
-            </h3>
-
-            <div class="text-16px font-400">
-                Promovăm standarde jurnalistice prin instruire, asistență, politici publice, cercetare și pledoarie. Pe parcursul anilor, CJI a lansat și a desfășurat diverse proiecte în vederea îmbunătățirii condițiilor de activitate a jurnaliștilor din Republica Moldova și a cadrului legal din domeniul media. La fel, CJI a întreprins multiple acțiuni în susținerea libertății presei sau pentru solidarizarea breslei și promovarea deontologiei profesionale.
-            </div>
-        </div>
-
-        <div class=" bg-faded-gray rounded-24px p-24px mt-16px">
-            <h3 class="flex items-center text-18px mb-8px">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <g clip-path="url(#clip0_92_1770)">
-                        <path opacity="0.2" d="M6.375 20.25C7.82475 20.25 9 19.0747 9 17.625C9 16.1753 7.82475 15 6.375 15C4.92525 15 3.75 16.1753 3.75 17.625C3.75 19.0747 4.92525 20.25 6.375 20.25Z" fill="#00665E"/>
-                        <path d="M6.375 20.25C7.82475 20.25 9 19.0747 9 17.625C9 16.1753 7.82475 15 6.375 15C4.92525 15 3.75 16.1753 3.75 17.625C3.75 19.0747 4.92525 20.25 6.375 20.25Z" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M3.75 6.75L7.5 10.5" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M7.5 6.75L3.75 10.5" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M16.5 15.75L20.25 19.5" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M20.25 15.75L16.5 19.5" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M12.75 7.5V3.75H16.5" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M12.75 3.75L14.25 5.25C18 9 15 13.5 12 14.25" stroke="#00665E" stroke-linecap="round" stroke-linejoin="round"/>
-                    </g>
-                    <defs>
-                        <clipPath id="clip0_92_1770">
-                        <rect width="24" height="24" fill="white"/>
-                        </clipPath>
-                    </defs>
-                </svg>
-                <span class="ml-4px inline-block text-18px font-600">
-                     Direcții Strategice               
-                </span>
-            </h3>
-
-            <div class="text-content">
-                <ul>
-                    <li>
-                        CJI susține jurnalismul profesionist și contribuie la îmbunătățirea standardelor profesionale ale jurnaliștilor, prin instruire și asistență;
-                    </li>
-                    <li>
-                        CJI contribuie la revizuirea și îmbunătățirea legislației media și monitorizează implementarea acesteia de către autoritățile responsabile, prin cercetare și elaborarea de politici publice;
-                    </li>
-                    <li>
-                        CJI promovează gândirea critică și oferă consumatorilor media instrumente și resursele necesare pentru a înțelege riscurile dezinformării, prin programe de educație media.
-                    </li>
-                </ul>
-
-                <p>
-                    Pe lângă evenimente tematice curente, menționăm și cele două evenimente importante ale anului, organizate de CJI și desfășurate sub egida acestuia: Zilele Libertății Presei (luna mai) şi Gala Presei (luna decembrie).            
-                </p>
-
-                <p>           
-                    Pe lângă site-ul <a href="#">www.cji.md</a>, CJI administrează alte trei portaluri: <a href="#">Media Azi</a>, unde sunt publicate știri din domeniul media; portalul <a href="#">Mediacritica</a>, menit să promoveze educația media și gândirea critică; portalul <a href="#">Moldova Azi</a>, o resursă multimedia, unde pot fi găsite și vizionate toate materialele video realizate până în prezent de către.          
-                </p>
-
-                <p>
-                    CJI este membru al Reţelei de profesionalizare a mass-media din Europa de Sud-Est (South East European Network for Professionalisation of the Media <a href="#">Media Azi</a>, unde sunt publicate știri din domeniul media; portalul <a href="#">SEENPM</a>), al <a href="#">IFEX</a>, precum și al Forumului Global pentru Dezvoltarea Mass-Media (Global Forum for Media Development <a href="#">GFMD</a>).
-                </p>
-
-                <p>
-                    Informații detaliate despre proiectele implementate sunt disponibile pe site-ul <a href="#">www.cji.md</a>, la rubrica Activitatea noastră.
-                </p>
-            </div>
-        </div>
     </div>
 </section>
 
