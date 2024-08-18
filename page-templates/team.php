d3b38e6475c0df25dc1ccb00db9523d953c86059
colealucas <colealucas@gmail.com>
2024-08-18 17:40:09 +0300

    updated team page

--- page-templates/team.php
+++ page-templates/team.php
@@ -9,135 +9,49 @@
 
 <section class="team-section py-40px md:py-72px">
     <div class="container">
-        <h1 class="text-36px md:text-64px text-center font-700"><span>Echipa</span></h1>
+        <h1 class="text-36px md:text-64px text-center font-700"><span><?php echo get_field('page_title'); ?></span></h1>
        
-        <div class="mt-40px md:mt-56px mb-24px">
-            <h2 class="text-28px md:text-32px font-700">Autoare</h2>
-        </div>
+        <?php if( have_rows('add_team_members') ) : ?>
+            <?php while( have_rows('add_team_members') ): the_row();
+                $title = get_sub_field('title');
+            ?>
 
-        <div class="row">
-            <div class="col-md-3">
-                <div class="mb-36px">
-                    <img class=" rounded-24px" src="https://via.placeholder.com/318x352" alt="">
-
-                    <h3 class="text-18px font-600 mt-16px">Solovei Constantin</h3>
-
-                    <p class="text-16px font-400 mt-8px">
-                        Product Designer
-                    </p>
-
-                    <p class="text-14px font-400 mt-8px">
-                        Founding design team at Figma. Former Pleo, Stripe and Tile
-                    </p>
+                <div class="mt-40px md:mt-56px mb-24px">
+                    <h2 class="text-28px md:text-32px font-700"><?php echo $title; ?></h2>
                 </div>
-            </div>
-            <div class="col-md-3">
-                <div>
-                    <img class=" rounded-24px" src="https://via.placeholder.com/318x352" alt="">
 
-                    <h3 class="text-18px font-600 mt-16px">Solovei Constantin</h3>
+                <?php if( have_rows('team_members') ): ?>
+                    <div class="row">
+                        <?php while( have_rows('team_members') ): the_row(); 
+                            $photo = get_sub_field('photo');
+                            $name = get_sub_field('name');
+                            $role = get_sub_field('role');
+                            $bio = get_sub_field('bio');
+                        ?>
+                            
+                            <div class="col-md-3">
+                                <div class="mb-36px">
+                                    <?php if ( $photo ) : ?>
+                                        <img class="rounded-24px" src="<?php echo $photo; ?>" alt="">
+                                    <?php endif; ?>
 
-                    <p class="text-16px font-400 mt-8px">
-                        Product Designer
-                    </p>
+                                    <h3 class="text-18px font-600 mt-16px"><?php echo $name; ?></h3>
 
-                    <p class="text-14px font-400 mt-8px">
-                        Founding design team at Figma. Former Pleo, Stripe and Tile
-                    </p>
+                                    <p class="text-16px font-400 mt-8px">
+                                        <?php echo $role; ?>
+                                    </p>
+
+                                    <p class="text-14px font-400 mt-8px">
+                                        <?php echo $bio; ?>
+                                    </p>
+                                </div>
+                            </div>
+                            
+                        <?php endwhile; ?>
                 </div>
-            </div>
-        
-        </div>
-
-        <div class="mt-56px mb-24px">
-            <h2 class="text-28px md:text-32px font-700">Dezvoltatori</h2>
-        </div>
-
-        <div class="row">
-            <div class="col-md-3">
-                <div class="mb-36px">
-                    <img class=" rounded-24px" src="https://via.placeholder.com/318x352" alt="">
-
-                    <h3 class="text-18px font-600 mt-16px">Solovei Constantin</h3>
-
-                    <p class="text-16px font-400 mt-8px">
-                        Product Designer
-                    </p>
-                    
-                    <p class="text-14px font-400 mt-8px">
-                        Founding design team at Figma. Former Pleo, Stripe and Tile
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="mb-36px">
-                    <img class=" rounded-24px" src="https://via.placeholder.com/318x352" alt="">
-
-                    <h3 class="text-18px font-600 mt-16px">Solovei Constantin</h3>
-
-                    <p class="text-16px font-400 mt-8px">
-                        Product Designer
-                    </p>
-
-                    <p class="text-14px font-400 mt-8px">
-                        Founding design team at Figma. Former Pleo, Stripe and Tile
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="mb-36px">
-                    <img class=" rounded-24px" src="https://via.placeholder.com/318x352" alt="">
-
-                    <h3 class="text-18px font-600 mt-16px">Solovei Constantin</h3>
-
-                    <p class="text-16px font-400 mt-8px">
-                        Product Designer
-                    </p>
-
-                    <p class="text-14px font-400 mt-8px">
-                        Founding design team at Figma. Former Pleo, Stripe and Tile
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div>
-                    <img class=" rounded-24px" src="https://via.placeholder.com/318x352" alt="">
-
-                    <h3 class="text-18px font-600 mt-16px">Solovei Constantin</h3>
-
-                    <p class="text-16px font-400 mt-8px">
-                        Product Designer
-                    </p>
-
-                    <p class="text-14px font-400 mt-8px">
-                        Founding design team at Figma. Former Pleo, Stripe and Tile
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="mt-56px mb-24px">
-            <h2 class="text-28px md:text-32px font-700">Redactor</h2>
-        </div>
-
-        <div class="row">
-            <div class="col-md-3">
-                <div>
-                    <img class=" rounded-24px" src="https://via.placeholder.com/318x352" alt="">
-
-                    <h3 class="text-18px font-600 mt-16px">Solovei Constantin</h3>
-
-                    <p class="text-16px font-400 mt-8px">
-                        Product Designer
-                    </p>
-                    
-                    <p class="text-14px font-400 mt-8px">
-                        Founding design team at Figma. Former Pleo, Stripe and Tile
-                    </p>
-                </div>
-            </div>
-           
-        </div>
+            <?php endif; ?> 
+            <?php endwhile; ?>
+        <?php endif; ?>
         
     </div>
 </section>
