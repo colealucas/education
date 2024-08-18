50ebed7511b30b7b99a53578e7355c72adc391d4
colealucas <colealucas@gmail.com>
2024-08-18 23:26:09 +0300

    updated 404

--- 404.php
+++ 404.php
@@ -10,51 +10,19 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<section class="error-404 not-found py-40px min-h-[60vh] flex flex-col justify-center">
+		<header class="page-header">
+			<h1 class="page-title text-36px font-700 text-center"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'education' ); ?></h1>
+		</header>
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'education' ); ?></h1>
-			</header><!-- .page-header -->
+		<div class="page-content text-center mt-16px">
+			<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'education' ); ?></p>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'education' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'education' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$education_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'education' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$education_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+			<p class="mt-30px">
+				<a href="#" class="btn">Homepage</a>
+			</p>
+		</div>
+	</section>
 
 <?php
 get_footer();
