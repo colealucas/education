5b202f0da5c7b898432efd12563892a9daed9ed6
colealucas <colealucas@gmail.com>
2024-08-18 21:23:46 +0300

    save notions

--- page-templates/vocabulary.php
+++ page-templates/vocabulary.php
@@ -4,7 +4,13 @@
  * 
  */
 
- get_header(); 
+ get_header();
+
+ $romanian_alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'î', 'j', 'k', 'l', 'm', 
+    'n', 'o', 'p', 'q', 'r', 's', 'ș', 't', 'ț', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
  ?>
 
 <section class="team-section py-40px md:py-72px">
@@ -13,143 +19,56 @@
         
         <div class="mt-24px">
             <div class="voc flex justify-between bg-faded-white rounded-8px w-full">
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px active" href="#">A</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">B</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">C</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">D</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">E</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">F</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">G</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">H</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">I</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">J</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">K</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">L</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">M</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">N</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">O</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">P</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">Q</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">R</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">S</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">Ș</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">T</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">Ț</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">U</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">V</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">W</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">X</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">Y</a>
-                <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#">Z</a>
+                <?php if(is_array($romanian_alphabet)) : $i=0; ?>
+                    <?php foreach($romanian_alphabet as $letter) : $i++; ?>
+                        <a class="text-16px text-dark leading-1 uppercase voc-letter block relative py-8px" href="#" data-voc-letter="<?php echo trim($letter); ?>" data-voc-filter-link><?php echo $letter; ?></a>
+                    <?php endforeach; ?>
+                <?php endif; ?>
             </div>
         </div>
 
         <div class="notions mt-24px">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="notion-box bg-faded-white p-24px rounded-16px mb-16px" data-notion-box>
-                        <div class="notion-title text-16px cursor-pointer select-none flex justify-between" data-notion-title>
-                            Articol
+            <?php if( have_rows('notions') ): ?>
+                <div class="row">
+                    <?php while( have_rows('notions') ): the_row(); 
+                        $letter = (get_sub_field('assign_letter') ? get_sub_field('assign_letter') : 'a'); // default to A letter
+                        $notion = get_sub_field('notion');
+                        $description = get_sub_field('description');
+                    ?>
+                        
+                        <div class="col-md-4" data-notion-box data-filter-letter="<?php echo $letter; ?>">
+                            <div class="notion-box bg-faded-white rounded-16px mb-16px pb-16px">
+                                <div class="notion-title text-16px cursor-pointer select-none flex justify-between p-24px pb-8px" data-notion-title>
+                                    <?php echo $notion; ?>
 
-                            <div class="notion-icon">
-                                <span class="plus">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g id="plus">
-                                            <path id="Icon" d="M9.99996 4.16663V15.8333M4.16663 9.99996H15.8333" stroke="#808285" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
-                                        </g>
-                                    </svg>
-                                </span>
+                                    <div class="notion-icon">
+                                        <span class="plus">
+                                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <g id="plus">
+                                                    <path id="Icon" d="M9.99996 4.16663V15.8333M4.16663 9.99996H15.8333" stroke="#808285" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                            </svg>
+                                        </span>
 
-                                <span class="minus">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g id="minus">
-                                        <path id="Icon" d="M4.16663 10H15.8333" stroke="#171412" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
-                                        </g>
-                                    </svg>
-                                </span>
+                                        <span class="minus">
+                                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <g id="minus">
+                                                <path id="Icon" d="M4.16663 10H15.8333" stroke="#171412" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div class="notion-body text-16 text-dark leading-150 mt-8px p-24px pt-0 content-links content-list content-spacing" style="display:none" data-notion-body>
+                                    <?php echo $description; ?>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="notion-body text-16 text-dark leading-150 mt-8px" style="display:none" data-notion-body>
-                            Material publicistic în care autorul/autoarea expune informații cu caracter politic, 
-                            social științific ș.a. De mai mici sau mai mari dimensiuni, un articol prezintă
-                            punctul de vedere al redacției (articol de fond), anumite principii 
-                            (articol-program, apărut în primul număr al unei publicații noi) 
-                            sau dezvoltă un subiect fie în formă de știre, fie în formă de reportaj. 
-                            Articolul poate avea un caracter descriptiv sau analitic.
-                        </div>
-                    </div>
+                        
+                    <?php endwhile; ?>
                 </div>
-                <div class="col-md-4">
-                    <div class="notion-box bg-faded-white p-24px rounded-16px mb-16px" data-notion-box>
-                        <div class="notion-title text-16px cursor-pointer select-none flex justify-between" data-notion-title>
-                            Articol
-
-                            <div class="notion-icon">
-                                <span class="plus">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g id="plus">
-                                            <path id="Icon" d="M9.99996 4.16663V15.8333M4.16663 9.99996H15.8333" stroke="#808285" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
-                                        </g>
-                                    </svg>
-                                </span>
-
-                                <span class="minus">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g id="minus">
-                                        <path id="Icon" d="M4.16663 10H15.8333" stroke="#171412" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
-                                        </g>
-                                    </svg>
-                                </span>
-                            </div>
-                        </div>
-
-                        <div class="notion-body text-16 text-dark leading-150 mt-8px" style="display:none" data-notion-body>
-                            Material publicistic în care autorul/autoarea expune informații cu caracter politic, 
-                            social științific ș.a. De mai mici sau mai mari dimensiuni, un articol prezintă
-                            punctul de vedere al redacției (articol de fond), anumite principii 
-                            (articol-program, apărut în primul număr al unei publicații noi) 
-                            sau dezvoltă un subiect fie în formă de știre, fie în formă de reportaj. 
-                            Articolul poate avea un caracter descriptiv sau analitic.
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="notion-box bg-faded-white p-24px rounded-16px mb-16px" data-notion-box>
-                        <div class="notion-title text-16px cursor-pointer select-none flex justify-between" data-notion-title>
-                            Articol
-
-                            <div class="notion-icon">
-                                <span class="plus">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g id="plus">
-                                            <path id="Icon" d="M9.99996 4.16663V15.8333M4.16663 9.99996H15.8333" stroke="#808285" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
-                                        </g>
-                                    </svg>
-                                </span>
-
-                                <span class="minus">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g id="minus">
-                                        <path id="Icon" d="M4.16663 10H15.8333" stroke="#171412" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
-                                        </g>
-                                    </svg>
-                                </span>
-                            </div>
-                        </div>
-
-                        <div class="notion-body text-16 text-dark leading-150 mt-8px" style="display:none" data-notion-body>
-                            Material publicistic în care autorul/autoarea expune informații cu caracter politic, 
-                            social științific ș.a. De mai mici sau mai mari dimensiuni, un articol prezintă
-                            punctul de vedere al redacției (articol de fond), anumite principii 
-                            (articol-program, apărut în primul număr al unei publicații noi) 
-                            sau dezvoltă un subiect fie în formă de știre, fie în formă de reportaj. 
-                            Articolul poate avea un caracter descriptiv sau analitic.
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php endif; ?>
         </div>
 
     </div>
