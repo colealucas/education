85400925aab967c1d412fa0579ee34e60410e6c1
colealucas <colealucas@gmail.com>
2024-08-29 21:09:53 +0300

    improved match definition task

--- template-parts/flexible-content/theme-content.php
+++ template-parts/flexible-content/theme-content.php
@@ -27,8 +27,19 @@
 
     <div class="flexible-content-section text-content-section my-12px">
         <?php if ( $section_title ) : ?>
-            <div class="tcs-heading mb-16px">
-                <h2 class="text-28px font-600"><?php echo $section_title; ?></h2>
+            <div class="tcs-heading mb-16px bg-green text-white py-20px px-24px rounded-16px">
+                <h2 class="text-22px font-500 flex items-center gap-12px">
+                    <span class="w-30px block">
+                        <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 17V11" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+                            <circle cx="1" cy="1" r="1" transform="matrix(1 0 0 -1 11 9)" fill="currentColor"/>
+                            <path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+                        </svg>
+                    </span>
+                    <span class="w-[calc(100%-42px)] block">
+                        <?php echo $section_title; ?>
+                    </span>
+                </h2>
             </div>
         <?php endif; ?>
 
@@ -57,15 +68,26 @@
 ?>
 
     <div class="flexible-content-section match-definitions my-24px">
-        <div class="theme-heading">
-            <h2><?php echo $section_title; ?></h2>
+        <div class="theme-heading bg-green text-white py-20px px-24px rounded-16px mb-16px">
+            <h2 class="text-22px font-500 flex items-center gap-12px">
+                <span class="w-30px block">
+                    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 17V11" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+                        <circle cx="1" cy="1" r="1" transform="matrix(1 0 0 -1 11 9)" fill="currentColor"/>
+                        <path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+                    </svg>
+                </span>
+                <span class="w-[calc(100%-42px)] block">
+                    <?php echo $section_title; ?>
+                </span>
+            </h2>
         </div>
 
         <div class="tcs-content phase-content entry-content content-spacing text-16px">
             <?php echo $section_description; // optional ?>
         </div>
 
-        <div class="match-definitions-wrap my-24px">
+        <div class="match-definitions-wrap my-24px flex flex-col gap-16px">
             <?php if( have_rows('definitions') ): ?>
                 <?php while( have_rows('definitions') ): the_row(); 
                     $word = get_sub_field('word');
@@ -74,13 +96,13 @@
                 
                     <div class="flex flex-nowrap gap-24px">
                         <div class="w-50% definitions-column">
-                            <div class="definition rounded-8px py-16px px-20px mb-16px bg-light-gray font-600 text-16px" data-word="<?php echo $word; ?>">
+                            <div class="definition rounded-8px py-16px px-20px bg-light-gray font-600 text-16px" data-word="<?php echo $word; ?>">
                                 <?php echo $def; ?>
                             </div>
                         </div>
 
                         <div class="w-50% placeholders-column flex items-center">
-                            <div class="placeholder rounded-8px mb-16px text-16px font-600 flex items-center justify-center w-full min-h-[40px]" data-word="<?php echo $word; ?>"></div>
+                            <div class="placeholder h-full rounded-8px text-16px font-600 flex items-center justify-center w-full min-h-[40px]" data-word="<?php echo $word; ?>"></div>
                         </div>
                     </div>
 
@@ -106,6 +128,11 @@
                     <?php if( !empty($words_data) ) : 
                         foreach( $words_data as $the_word ) : ?>
                             <div class="word inline-flex items-center justify-center max-w-[50%] p-8px text-center rounded-8px text-16px font-600 leading-120 border-medium-gray border-2px border-solid bg-white" draggable="true" data-word="<?php echo $the_word; ?>">
+                                <span class="inline-block mr-6px text-gray">
+                                    <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12.0001 2.89331L8.81809 6.07529L9.87875 7.13595L11.2501 5.76463V11.2499H5.7649L7.13619 9.8786L6.07553 8.81794L2.89355 11.9999L6.07553 15.1819L7.13619 14.1212L5.76485 12.7499H11.2501V18.2352L9.87875 16.8639L8.81809 17.9245L12.0001 21.1065L15.182 17.9245L14.1214 16.8639L12.7501 18.2352V12.7499H18.2353L16.8639 14.1213L17.9246 15.1819L21.1066 11.9999L17.9246 8.81796L16.8639 9.87862L18.2352 11.2499H12.7501V5.76463L14.1214 7.13595L15.182 6.07529L12.0001 2.89331Z" fill="currentColor"/>
+                                    </svg>
+                                </span>
                                 <?php echo $the_word; ?>
                             </div>
                         <?php endforeach; ?>
