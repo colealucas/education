0ab55f8701fedf75c8e6371d9f6c0d7fe7000fee
colealucas <colealucas@gmail.com>
2024-10-01 21:26:46 +0300

    css fix

--- single-module.php
+++ single-module.php
@@ -143,14 +143,14 @@ $current_module_title = '';
                                         </div>
                                     </div>
                                     <div>
-                                        <a href="<?php the_permalink(); ?>" class="block hover:text-green font-500 min-w-[200px]">
+                                        <a href="<?php the_permalink(); ?>" class="block hover:text-green font-500">
                                             <h3 class="text-30px leading-110 pl-16px"><?php the_title(); ?></h3>
                                         </a>
                                     </div>
                                 </div>
                             </div>
                             <div>
-                                <a href="<?php the_permalink(); ?>" class="btn">
+                                <a href="<?php the_permalink(); ?>" class="btn min-w-[200px]">
                                     <?php echo $get_static_text[get_lang()]['access_theme']; ?>
                                     <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M1.5 11L6.5 6L1.5 1" stroke="currentColor" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
