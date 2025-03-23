7b75f5ee7ca797039e24c35f804c482b508ac8a3
colealucas <colealucas@gmail.com>
2025-03-23 15:02:27 +0200

    fix: add check for tinymce existence before initializing custom plugin to prevent errors

--- assets/js/custom-mce-plugin.js
+++ assets/js/custom-mce-plugin.js
@@ -1,18 +1,20 @@
 (function() {
-    tinymce.PluginManager.add('custom_class_plugin', function(editor, url) {
-        editor.addButton('custom_class_button', {
-            text: '',
-            icon: 'mce-ico mce-i-notice',
-            tooltip: 'Ignore paragraph', // Added tooltip text
-            onclick: function() {
-                let selectedText = editor.selection.getContent({format: 'html'});
+    if (typeof tinymce !== 'undefined') {
+        tinymce.PluginManager.add('custom_class_plugin', function(editor, url) {
+            editor.addButton('custom_class_button', {
+                text: '',
+                icon: 'mce-ico mce-i-notice', 
+                tooltip: 'Ignore paragraph', // Added tooltip text
+                onclick: function() {
+                    let selectedText = editor.selection.getContent({format: 'html'});
 
-                if (selectedText) {
-                    editor.insertContent('<p class="ignore">' + selectedText + '</p>');
-                } else {
-                    alert('Selecteaza un paragraf mai intai :)');
+                    if (selectedText) {
+                        editor.insertContent('<p class="ignore">' + selectedText + '</p>');
+                    } else {
+                        alert('Selecteaza un paragraf mai intai :)');
+                    }
                 }
-            }
+            });
         });
-    });
+    }
 })();
