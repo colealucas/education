a90eb14c9985f88490e2bc74e1a30ffecbd30236
colealucas <colealucas@gmail.com>
2025-04-06 14:13:18 +0300

    fix: rename div class from 'page-entry-content' to 'entry-content' in content-page.php for improved semantic clarity

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -14,7 +14,7 @@
 		<h1 class="text-44px font-700 leading-130 max-w-[1000px] mx-auto text-center"><?php the_title(); ?></h1>
 	</header>
 
-	<div class="page-entry-content page-content min-h-[35vh] max-w-[1000px] mx-auto grren-headings">
+	<div class="entry-content page-entry-content page-content min-h-[35vh] max-w-[1000px] mx-auto grren-headings">
 		<?php the_content(); ?>
 	</div>
 </article>
