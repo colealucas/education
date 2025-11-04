77b17bbd0fc05f96966153a3769cf290d842415c
colealucas <colealucas@gmail.com>
2025-11-04 20:06:06 +0200

    fix: add print button with translations in Romanian and Russian for enhanced user experience in single-module.php

--- single-module.php
+++ single-module.php
@@ -7,6 +7,7 @@ get_header();
 
 $get_static_text = [
     'ro' => [
+        'print' => 'Printează',
         'subjects' => 'Subiecte',
         'go_back' => 'Înapoi la Module',
         'access_theme' => 'Accesează Tema',
@@ -20,6 +21,7 @@ $get_static_text = [
         'explore_subjects' => 'Explorează Subiectele',
     ],
     'ru' => [
+        'print' => 'Распечатать',
         'subjects' => 'Темы',
         'go_back' => 'Назад к модулям',
         'access_theme' => 'Открыть Тему',
@@ -201,10 +203,21 @@ $current_module_title = '';
                             </div>
 
                             <div class="sum-module-body hide">
-                                <div class="py-20px text-dark text-16px overflow-x-auto">
+                                <div class="py-20px text-dark text-16px overflow-x-auto print-div">
                                     <table class="text-left cells-border table-spacing border-0">
                                         <tr>
-                                            <th style="border:0"></th>
+                                            <th style="border:0; padding: 0;">
+                                                <div class="print-trigger-wrap flex" data-print-trigger="criteria">
+                                                    <a href="#" class="inline-flex leading-1 bg-light-gray py-4px px-20px rounded-8px text-15px font-600 items-center gap-4px print-trigger" data-print-trigger="">
+                                                        <span>
+                                                            <svg fill="#000000" width="30px" height="30px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                                                <path d="M5.656 6.938l-0.344 2.688h11.781l-0.344-2.688c0-0.813-0.656-1.438-1.469-1.438h-8.188c-0.813 0-1.438 0.625-1.438 1.438zM1.438 11.094h19.531c0.813 0 1.438 0.625 1.438 1.438v8.563c0 0.813-0.625 1.438-1.438 1.438h-2.656v3.969h-14.219v-3.969h-2.656c-0.813 0-1.438-0.625-1.438-1.438v-8.563c0-0.813 0.625-1.438 1.438-1.438zM16.875 25.063v-9.281h-11.344v9.281h11.344zM15.188 18.469h-8.125c-0.188 0-0.344-0.188-0.344-0.375v-0.438c0-0.188 0.156-0.344 0.344-0.344h8.125c0.188 0 0.375 0.156 0.375 0.344v0.438c0 0.188-0.188 0.375-0.375 0.375zM15.188 21.063h-8.125c-0.188 0-0.344-0.188-0.344-0.375v-0.438c0-0.188 0.156-0.344 0.344-0.344h8.125c0.188 0 0.375 0.156 0.375 0.344v0.438c0 0.188-0.188 0.375-0.375 0.375zM15.188 23.656h-8.125c-0.188 0-0.344-0.188-0.344-0.375v-0.438c0-0.188 0.156-0.344 0.344-0.344h8.125c0.188 0 0.375 0.156 0.375 0.344v0.438c0 0.188-0.188 0.375-0.375 0.375z"></path>
+                                                            </svg>
+                                                        </span>
+                                                        <?php echo $get_static_text[get_lang()]['print']; ?>
+                                                    </a>
+                                                </div>
+                                            </th>
                                             <th style="border:0"></th>
                                             <th class="text-center border-0" style="border:0">
                                                 <svg style="transform: scaleX(-1);" class="inline-block" width="50px" height="50px" viewBox="0 0 1024 1024" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg">
